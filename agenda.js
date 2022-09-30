8393161e07a121950e70973a890dcb5d7c22b4e5
guilhermeinaia <guilherme.inaias@gmail.com>
2022-09-30 20:08:35 -0300

    Atualizando informações.

--- agenda.js
+++ agenda.js
@@ -1,32 +1,32 @@
-fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
-    .then((resposta) => resposta.json())
-    .then((regioes) =>  {
-        regioes.forEach((cadaRegiao) => {
-            document.getElementById('region').innerHTML += `
-            <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>
-            `;        
-        });
-    }) 
+// fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
+//     .then((resposta) => resposta.json())
+//     .then((regioes) =>  {
+//         regioes.forEach((cadaRegiao) => {
+//             document.getElementById('region').innerHTML += `
+//             <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>
+//             `;        
+//         });
+//     }) 
 
-    function buscarEstado(){
-        let id_estado = document.getElementById('region').value
-        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id_estado}/estados`)
-        .then((resposta) => resposta.json())
-        .then((estado) => {
-                document.getElementById('states').innerHTML = " "
-                estado.forEach((cadaEstado) => {
-                    document.getElementById('states').innerHTML += `
-                    <option value="${cadaEstado.id}">${cadaEstado.nome}</option>
-                    `;
+//     function buscarEstado(){
+//         let id_estado = document.getElementById('region').value
+//         fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id_estado}/estados`)
+//         .then((resposta) => resposta.json())
+//         .then((estado) => {
+//                 document.getElementById('states').innerHTML = " "
+//                 estado.forEach((cadaEstado) => {
+//                     document.getElementById('states').innerHTML += `
+//                     <option value="${cadaEstado.id}">${cadaEstado.nome}</option>
+//                     `;
 
-             });
+//              });
         
-        });
-    }
+//         });
+//     }
 
     function buscarCidade(){
         let id_cidade = document.getElementById('states').value
-        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id_cidade}/microrregioes`)
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/microrregioes`)
         .then((resposta) => resposta.json())
         .then((cidade) => {
                 document.getElementById('city').innerHTML = " "
@@ -40,16 +40,16 @@ fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
         });
     }
 
-    function buscarBairro() {
-        let id_bairro = document.getElementById('city').value 
-        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes/${id_bairro}/subdistritos`) 
-        .then((resposta) => resposta.json())
-        .then((bairro) => {
-            document.getElementById('district').innerHTML = " "
-            bairro.forEach((cadaBairro) => {
-                document.getElementById('district').innerHTML += `
-                <option value="${cadaBairro.id}">${cadaBairro.nome}</option>
-                `;
-            });
-        });
-    }
+    // function buscarBairro() {
+    //     let id_bairro = document.getElementById('city').value 
+    //     fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes/${id_bairro}/subdistritos`) 
+    //     .then((resposta) => resposta.json())
+    //     .then((bairro) => {
+    //         document.getElementById('district').innerHTML = " "
+    //         bairro.forEach((cadaBairro) => {
+    //             document.getElementById('district').innerHTML += `
+    //             <option value="${cadaBairro.id}">${cadaBairro.nome}</option>
+    //             `;
+    //         });
+    //     });
+    // }
